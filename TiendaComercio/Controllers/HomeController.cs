5ef4fc621eb42407ee2b91ad76aa330ce55994d1
TaliaBarrientosC <talia.barrientos.27@unsch.edu.pe>
2017-07-11 21:16:35 -0500

    nuevooos cvambios

--- TiendaComercio/Controllers/HomeController.cs
+++ TiendaComercio/Controllers/HomeController.cs
@@ -20,6 +20,7 @@ namespace TiendaComercio.Controllers
             //logica de acceso a bd
             var productos = bd.Producto.Where(x=>x.Descripcion.Contains(id)).Take(20).ToList();
             //lista de productos
+            ViewBag.clave = id;
             return View(productos);
         }
         public ActionResult Producto(string id)
